2d770ef94e2458360d1e5f7eed344029ceea1a3e
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2024-12-30 10:51:36 +0000

    Write a function and get request by id

--- controllers/breathingController.js
+++ controllers/breathingController.js
@@ -5,15 +5,26 @@ import path from "path"; // importing path module - helps manage and work with f
 
 // Dynamically load the JSON file
 const __dirname = path.resolve(); // __dirname: A variable that holds this path, representing the current directory //Returns the absolute path of the directory where the script is running.
-const filePath = path.join(__dirname, "breathingDataset.json");
+const filePath = path.join(__dirname, "./libs/breathingDataset.json");
 //path.join(): Combines multiple path segments into a single path string.
 // __dirname: The base path
 // "breathingDataset.json": The filename to access.
 const data = JSON.parse(fs.readFileSync(filePath, "utf8")); //Reads the file and returns the content as a string //data stores the parsed JSON object or array, which can now be worked with
 
+const techniques = data; //assigns techniques as the variable to store the data from the array
 //the code for the functions for each request will go here
 
 // 1. Function to get all breathing techniques
 export async function getAllTechniques() {
     return [...data];
+}
+
+// 2. Function to get a single breathing technique
+//create an async function to get a technique by ID, it will have one parameter
+export async function getTechniqueByID(id) {
+//find the technique with the matching id
+return techniques.find(function(technique) {
+//check if the id provided is equal to the id
+return technique.id === id;
+});
 }
--- routes/breathingRoutes.js
+++ routes/breathingRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'; //imports the express module from the express package
 const router = express.Router(); //initialises an express router, the router variable can now be used to define routes and middleware for handling HTTP requests
-import { getAllTechniques } from '../controllers/breathingController.js';
+import { getAllTechniques, getTechniqueByID } from '../controllers/breathingController.js';
 
 // Routes 
 //1. GET reqeust to get all breathing techniques
@@ -8,11 +8,27 @@ router.get('/', async function (req,res) {
     const returnedData = await getAllTechniques();
     const allTechniques = {
         "success": true,
-        "payload": returnedData,
+        "data": returnedData,
       }
       //respond to the client with all techniques
       res.json(allTechniques);
     } )
      //GET request to the /breathing endpoint, calls the getAllTechniques function from the breathingController
+  
+  //2. GET request to get a breathing technique by ID
+  //create a get request for a unique id using an async func
+  router.get("/:id", async function (req, res) {
+  //use getTechniqueByID function to get the specific id
+  //specify that the id parameters are in the URI and data type is a number
+  let returnedData = await getTechniqueByID(Number(req.params["id"]));
+  //follow response specification
+  const idTechnique = {
+    "success": true,
+    "data": returnedData
+  }
+  console.log(idTechnique)
+  //respond to the client technique with corresponding id
+  res.json(idTechnique);
+  })
 
 export default router; //exports the router object
