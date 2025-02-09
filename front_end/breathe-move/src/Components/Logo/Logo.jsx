e9b255be81c884d3c350ca1608844b522fc92fd1
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-02-09 00:27:34 +0000

    Updates to file structure

--- front_end/breathe-move/src/Components/Logo/Logo.jsx
+++ front_end/breathe-move/src/Components/Logo/Logo.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import logo from '../../Images/wellness-logo.jpeg';
+import logo from '../../Images/wellness-logo.png';
 
 function Logo({className}) {
     return <img src={logo} alt="Logo containing a wave and leave style drawing" className={className} />
