e8e5adfc94155ae9cb29c27b42f5850809c72132
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-02-09 19:25:48 +0000

    update to github logo for deployment

--- front_end/breathe-move/src/Components/Footer/Footer.jsx
+++ front_end/breathe-move/src/Components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import styles from "../Footer/Footer.module.css"
+import logo from "../../Images/github-logo.png"
 
 function Footer() {
     return (
     <div className={styles.container}>
-    <p className={styles.text}>Created by Grace O Reilly  <a href="https://github.com/graceoreilly"><img src="/src/Images/github-logo.png" alt="github logo" className={styles.githubLogo} /></a></p>
+    <p className={styles.text}>Created by Grace O Reilly  <a href="https://github.com/graceoreilly"><img src={logo} alt="github logo" className={styles.githubLogo} /></a></p>
     </div>
     )
 }
