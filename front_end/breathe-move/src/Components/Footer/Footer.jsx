f8fa3e9e01f5adef3898967c1986982202461dd5
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-02-09 19:18:06 +0000

    Styling added to footer

--- front_end/breathe-move/src/Components/Footer/Footer.jsx
+++ front_end/breathe-move/src/Components/Footer/Footer.jsx
@@ -1,5 +1,11 @@
+import styles from "../Footer/Footer.module.css"
+
 function Footer() {
-    return <p>Created by Grace O Reilly</p>;
+    return (
+    <div className={styles.container}>
+    <p className={styles.text}>Created by Grace O Reilly  <a href="https://github.com/graceoreilly"><img src="/src/Images/github-logo.png" alt="github logo" className={styles.githubLogo} /></a></p>
+    </div>
+    )
 }
 
 export default Footer;
