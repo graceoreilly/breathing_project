9e56626f4425a75fb0e954bb19fe50b082cab540
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-03-08 10:56:58 +0000

    ensure logo brings user to homepage

--- front_end/breathe-move/src/Components/Logo/Logo.jsx
+++ front_end/breathe-move/src/Components/Logo/Logo.jsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/prop-types */
-import logo from '../../Images/wellness-logo.png';
+import logo from "../../Images/wellness-logo.png";
+import { Link } from "react-router-dom";
 
-function Logo({className}) {
-    return <img src={logo} alt="Logo containing a wave and leave style drawing" className={className} />
+function Logo({ className }) {
+  return (
+    <>
+    <Link to="/">
+    <img
+      src={logo}
+      alt="Logo containing a wave and leave style drawing"
+      className={className}
+    />
+    </Link>
+    </>
+  );
 }
 
 export default Logo;
