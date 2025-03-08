af79c1c86d11150a4224457f86324c7997fd3c6e
“graceoreillygit config --global user.email “graceoreilly111@gmail.com <yourname@example.comf>
2025-03-08 10:54:12 +0000

    add functionality to homepage

--- front_end/breathe-move/src/Components/Header/Header.jsx
+++ front_end/breathe-move/src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import styles from "../Header/Header.module.css";
 import Button from '../../Components/Button/Button.jsx';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
@@ -7,8 +8,12 @@ function Header() {
       <div className={styles.container}>
         <h1 className={styles.heading}>Breathe. Move. Repeat.</h1>
         <div className={styles.buttonContainer}>
+          <Link to="/breathwork">
         <Button buttonText="Breathwork" />
+        </Link>
+        <Link to="/yoga">
         <Button buttonText="Build a yoga flow" />
+        </Link>
         </div>
       </div>
     </>
